4f0cf918d6639d08307d0d575febfa9b6045cf46
Franciele Zanella <36767316+Franzanella@users.noreply.github.com>
2020-07-31 12:53:44 -0300

    vercel

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -19,7 +19,7 @@ function CadastroCategoria() {
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
       ? 'http://localhost:8080/categorias'
-      : 'https://franflix.herokuapp.com/';
+      : '';
     
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
