4047bf41ae13dfd3ce950a9fbe2e6647c794f20f
Franciele Zanella <36767316+Franzanella@users.noreply.github.com>
2020-07-31 13:25:10 -0300

    teste 2

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
 ? 'http://localhost:8080'
-: '';
+: 'https://franflix.vercel.app/';
 export default {
   URL_BACKEND_TOP,
 };
+
